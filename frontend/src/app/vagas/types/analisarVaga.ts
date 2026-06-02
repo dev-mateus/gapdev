453a87914b06d079e6d875259878b530fa181513
Angela Melina Silva Chaves <1062981@senacminas.edu.br>
2026-06-02 08:05:54 -0300

    botões senioridade

--- frontend/src/app/vagas/types/analisarVaga.ts
+++ frontend/src/app/vagas/types/analisarVaga.ts
@@ -1,6 +1,7 @@
 export type AnalyzeJobRequest = {
   title: string
   company: string
+  seniority: string
   description: string
 }
 
@@ -27,5 +28,6 @@ export type AnalyzeJobAiResponse = {
 export type AnalyzeJobFormState = {
   title: string
   company: string
+  seniority: string
   description: string
 }
